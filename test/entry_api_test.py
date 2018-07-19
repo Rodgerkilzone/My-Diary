cf57a2812e0cde04ca11495302ee247215e927fc
rodger kilonzo <rodgerkilonzo@gmail.com>
2018-07-19 21:30:53 +0300

    [TEST]test for posting entry

--- test/entry_api_test.py
+++ test/entry_api_test.py
@@ -4,6 +4,7 @@ class TestApp(unittest.TestCase):
      def Variables(self):
          self.api_route = '/mydiary/api/v1/entries'
          self.api_route_2 = '/mydiary/api/v1/entries/1'
+         self.payload = {'title':'beach','content':'today i had fun at the beach'}
          self.entry = {
                             "id":1,
                             'title':"airport",
@@ -23,7 +24,14 @@ class TestApp(unittest.TestCase):
             # self.api_route_2= '/my_diary/api/v1/entries/1'
         response = self.tester.get(self.api_route_2)
         self.assertEqual(response.status_code, 200)
-        
+ # TEST FOR ENTRY POST
+     def test_entry_post(self):
+        response = self.tester.post(self.api_route,data=json.dumps(self.payload),
+        content_type="application/json")
+        self.assertEquals(response.status_code,201)
+        response_message = json.loads(response.data.decode('utf8'))
+        self.assertEqual("today i had fun at the beach",response_message['content'])
+
 
 
 if __name__ == "__main__":
