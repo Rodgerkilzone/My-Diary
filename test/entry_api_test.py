db523d4aec23329eb6995de6fc8a48a813a68c57
rodger kilonzo <rodgerkilonzo@gmail.com>
2018-07-19 21:17:05 +0300

    [TEST]test for getting entry by id

--- test/entry_api_test.py
+++ test/entry_api_test.py
@@ -1,13 +1,29 @@
 import unittest,json 
 
 class TestApp(unittest.TestCase):
-
-    def test_get_all_entries(self):
-        self.api_route = '/my_diary/api/v1/entries'
-        response = self.tester.get(self.api_route)
+     def Variables(self):
+         self.api_route = '/mydiary/api/v1/entries'
+         self.api_route_2 = '/mydiary/api/v1/entries/1'
+         self.entry = {
+                            "id":1,
+                            'title':"airport",
+                            'content':'i love travelling and adventure',
+                            "date":'5-8-2018'
+            }
+         Entry().save(self.entry)
+# TEST FOR GETTING ALL ENTRIES
+     def test_get_all_entries(self):
+            # self.api_route = '/my_diary/api/v1/entries'
+         response = self.tester.get(self.api_route)
+         self.assertEqual(response.status_code, 200)
+         self.assertIn("All Entries", str(response.data))
+         self.assertIn('application/json', str(response.headers))
+# TEST FOR GETTING SPECIFIC ENTRY BY ID
+     def test_get_entry_by_id(self):
+            # self.api_route_2= '/my_diary/api/v1/entries/1'
+        response = self.tester.get(self.api_route_2)
         self.assertEqual(response.status_code, 200)
-        self.assertIn("All Entries", str(response.data))
-        self.assertIn('application/json', str(response.headers))
+        
 
 
 if __name__ == "__main__":
